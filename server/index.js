33a24002225ab653809215d973826abb48de2242
Manuel Labarca <manuel.labarca@walmart.com>
2020-05-25 23:54:48 -0400

    remove ngrok

--- server/index.js
+++ server/index.js
@@ -2,10 +2,6 @@
 
 var port = 3410;
 var app = require('./app');
-const ngrok = require('ngrok');
-(async function() {
-  const url = await ngrok.connect();
-})();
 app.listen(port, () => {
     console.log("Servidor de node, corriendo en el puerto: " + port);
 
